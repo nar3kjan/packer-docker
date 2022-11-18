7ad74f88f5aba6175ab4e582097c48f415cd68c6
Narek <zzz@zzz.zzz>
2022-11-18 21:46:01 +0400

    pd

--- setup.sh
+++ setup.sh
@@ -1,23 +1,16 @@
 sudo apt-get update
-yes | sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
-yes | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo usermod -aG docker $user
+sudo snap install docker
+sudo addgroup --system docker
+sudo adduser ubuntu docker
+newgrp docker
+sudo snap disable docker
+sudo snap enable docker
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-yes | sudo ./aws/install
+sudo apt install unzip
+sudo unzip awscliv2.zip
+sudo ./aws/install
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 558664324013.dkr.ecr.us-east-1.amazonaws.com
+sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 558664324013.dkr.ecr.us-east-1.amazonaws.com
 
-docker run -d -p80:80 558664324013.dkr.ecr.us-east-1.amazonaws.com/ubuntu-nginx:latest
+sudo docker run -d -p80:80 558664324013.dkr.ecr.us-east-1.amazonaws.com/ubuntu-nginx:latest
