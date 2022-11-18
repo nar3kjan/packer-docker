fcd9a9d3988f9ad7acb30d4824bf949a1e29d053
Narek <zzz@zzz.zzz>
2022-11-18 23:35:26 +0400

    dd

--- setup.sh
+++ setup.sh
@@ -11,6 +11,9 @@ sudo apt install unzip
 sudo unzip awscliv2.zip
 sudo ./aws/install
 
+export AWS_ACCESS_KEY_ID=AKIAYEEX5COWSW5M6JKY
+export AWS_SECRET_ACCESS_KEY=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
+
 sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 558664324013.dkr.ecr.us-east-1.amazonaws.com
 
 sudo docker run -d -p80:80 558664324013.dkr.ecr.us-east-1.amazonaws.com/ubuntu-nginx:latest
